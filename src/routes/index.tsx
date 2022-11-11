77cd5504e0d6ca560879c6d7e450feb67c54a243
mg <mag190033@gmail.com>
2022-11-11 17:38:48 +0100

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+      <h2>Countdown Timer</h2>
+
+      <Link class="mindblow" href="/flower">
+        Blow my mind 🤯
+      </Link>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Countdown Timer',
+  meta: [
+    {
+      name: 'description',
+      content: 'Countdown Timer Description',
+    },
+  ],
+};
